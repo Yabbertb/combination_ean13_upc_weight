7d32a4f050a8d1a3df6071834e5dbc8f04151ddc
Yabbertb <144372521+Yabbertb@users.noreply.github.com>
2026-03-20 08:05:29 +0100

    Language-dependent weight format

--- themes/mytheme/js/product.js
+++ themes/mytheme/js/product.js
@@ -722,9 +722,9 @@ function updateDisplay() {
   if (selectedCombination['weight'] || productWeight) {
     if (selectedCombination['weight']) {
       productWeightSum = Number(productWeight) + Number(selectedCombination['weight']);
-      $productWeight.find('span').text(number_format(productWeightSum, 3));
+      $productWeight.find('span').text(number_format(productWeightSum));
     } else if (productWeight) {
-      $productWeight.find('span').text(number_format(productWeight, 3));
+      $productWeight.find('span').text(number_format(productWeight));
     }
     $productWeight.show();
   } else {
@@ -736,25 +736,12 @@ function updateDisplay() {
   }
 }
 
-function number_format(number, decimals, dec_point, thousands_point) {
+// Language-dependent weight format
+function number_format(number) {
   if (number == null || !isFinite(number)) {
     throw new TypeError("Number is not valid: " + number);
   }
-  if (!decimals) {
-    var len = number.toString().split('.').length;
-    decimals = len > 1 ? len : 0;
-  }
-  if (!dec_point) {
-    dec_point = ',';
-  }
-  if (!thousands_point) {
-    thousands_point = ' ';
-  }
-  number = parseFloat(number).toFixed(decimals);
-  number = number.replace(".", dec_point);
-  var splitNum = number.split(dec_point);
-  splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_point);
-  number = splitNum.join(dec_point);
+  number = new Intl.NumberFormat(langIso, {minimumFractionDigits: 3}).format(number);
   return number;
 }
 
